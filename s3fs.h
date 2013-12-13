0bea01d49fb5d948bc1480ce8bfa74c228288210
farahfouladi <ffouladi@colgate.edu>
2013-12-13 12:59:04 -0500

    Update s3fs.h
    added structs

--- s3fs.h
+++ s3fs.h
@@ -6,7 +6,7 @@
 #include <sys/time.h> // for struct timeval
 
 /* This code is based on the fine code written by Joseph Pfeiffer for his
-   fuse system tutorial. */
+  fuse system tutorial. */
 
 /* Declare to the FUSE API which version we're willing to speak */
 #define FUSE_USE_VERSION 26
@@ -19,15 +19,26 @@
 
 // store filesystem state information in this struct
 typedef struct {
-    char s3bucket[BUFFERSIZE];
+   char s3bucket[BUFFERSIZE];
 } s3context_t;
 
 /*
- * Other data type definitions (e.g., a directory entry
- * type) should go here.
- */
+* Other data type definitions (e.g., a directory entry
+* type) should go here.
+*/
 
-
+typedef struct {
+char type; // file, directory, or usused
+char* name;
+mode_t protection;
+uid_t user_id;
+gid_t group_id;
+nlink_t hard_links;
+off_t size;
+time_t last_access;
+time_t mod_time;
+time_t status_change;
+} s3dirent_t;
 
 
 #endif // __USERSPACEFS_H__
